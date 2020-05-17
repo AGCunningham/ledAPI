b2f1141b96246d79c9b6fa0898e9ccb1adbdbd8e
Adam Cunningham <ac11g13@soton.ac.uk>
2020-05-17 19:15:45 +0100

    Initialise GPIO (#1)
    * initial setup
    
    * working GPIO
    
    * output to console

--- ledcontrols/setting.go
+++ ledcontrols/setting.go
@@ -2,6 +2,8 @@ package ledcontrols
 
 import (
 	"fmt"
+
+	"github.com/stianeikeland/go-rpio/v4"
 )
 
 func SetLEDColours(colour string){
@@ -10,4 +12,19 @@ func SetLEDColours(colour string){
 
 func SetLEDPreset(preset string){
 	fmt.Println("Preset from the API is: "+preset)
+}
+
+func SetPinOut(set string){
+	// Pin out -> https://github.com/stianeikeland/go-rpio/blob/master/rpio.go
+	dataPin := rpio.Pin(26)
+	dataPin.Output()
+	dataPin.High()
+
+	fmt.Println("Pin has been set: "+set)
+
+	if set == "high" {
+		dataPin.High()
+	} else if set == "low" {
+		dataPin.Low()
+	}
 }
--- main.go
+++ main.go
@@ -3,15 +3,24 @@ package main
 import (
 	"log"
 	"net/http"
-	
+
 	"github.com/gorilla/mux"
+	"github.com/stianeikeland/go-rpio/v4"
+
 	server "ledAPI/webserver"
 )
 
 func main() {
+	err := rpio.Open()
+	defer rpio.Close()
+	if err != nil {
+		log.Fatal("GPIO Pins could not be initialised")
+	}
+
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/", server.HomeLink)
 	router.HandleFunc("/colour/{colour}", server.GetColour).Methods("GET")
 	router.HandleFunc("/preset/{preset}", server.SetPreset).Methods("GET")
+	router.HandleFunc("/pin/{set}", server.SetPin).Methods("GET")
 	log.Fatal(http.ListenAndServe(":10000", router))
 }

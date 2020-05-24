d727954e6a2876f90254981dbd1c73361d5f492a
Adam Cunningham <ac11g13@soton.ac.uk>
2020-05-24 16:50:43 +0100

    initial testing - do LEDs turn on

--- main.go
+++ main.go
@@ -5,17 +5,42 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
-	"github.com/stianeikeland/go-rpio/v4"
+	// "github.com/stianeikeland/go-rpio/v4"
+
+	// Initial Testing of LEDs
+	github.com/mcuadros/go-rpi-ws281x
 
 	server "ledAPI/webserver"
 )
 
 func main() {
-	err := rpio.Open()
-	defer rpio.Close()
-	if err != nil {
-		log.Fatal("GPIO Pins could not be initialised")
-	}
+	// err := rpio.Open()
+	// defer rpio.Close()
+	// if err != nil {
+	// 	log.Fatal("GPIO Pins could not be initialised")
+	// }
+
+	// create a new canvas with the given width and height, and the config, in this
+	// case the configuration is for a Unicorn pHAT (8x4 pixels matrix) with the default config
+	
+	// default configuration
+	// Pin:        18,
+	// Frequency:  800000, // 800khz
+	// DMA:        10,
+	// Brightness: 30,
+	// StripType:  StripGRB,
+
+	c, _ := ws281x.NewCanvas(150, 1, &ws281x.DefaultConfig)
+
+	// initialize the canvas and the matrix
+	c.Initialize()
+	defer c.Close()
+
+	// now we copy a white image into the ws281x.Canvas, this turn on all the leds to white
+	draw.Draw(c, c.Bounds(), image.NewUniform(color.White), image.ZP, draw.Over)
+	
+	// Render required to turn the LEDs on
+	c.Render()
 
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/", server.HomeLink)

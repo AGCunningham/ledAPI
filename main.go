92d1a62685f564500a46a3e5cccee98ac9b67cbb
Adam Cunningham <ac11g13@soton.ac.uk>
2020-05-27 20:27:44 +0100

    temp:check

--- main.go
+++ main.go
@@ -1,27 +1,109 @@
+// package main
+
+// import (
+// 	"log"
+// 	"net/http"
+
+// 	"github.com/gorilla/mux"
+// 	"github.com/stianeikeland/go-rpio/v4"
+
+// 	server "ledAPI/webserver"
+// )
+
+// func main() {
+// 	err := rpio.Open()
+// 	defer rpio.Close()
+// 	if err != nil {
+// 		log.Fatal("GPIO Pins could not be initialised")
+// 	}
+
+// 	router := mux.NewRouter().StrictSlash(true)
+// 	router.HandleFunc("/", server.HomeLink)
+// 	router.HandleFunc("/colour/{colour}", server.GetColour).Methods("GET")
+// 	router.HandleFunc("/preset/{preset}", server.SetPreset).Methods("GET")
+// 	router.HandleFunc("/pin/{set}", server.SetPin).Methods("GET")
+// 	router.HandleFunc("/pcon", server.PowerOn).Methods("GET")
+// 	log.Fatal(http.ListenAndServe(":10000", router))
+// }
+
+// Copyright 2018 Jacques Supcik / HEIA-FR
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package main
 
 import (
-	"log"
-	"net/http"
+	"time"
 
-	"github.com/gorilla/mux"
-	"github.com/stianeikeland/go-rpio/v4"
-
-	server "ledAPI/webserver"
+	ws2811 "github.com/rpi-ws281x/rpi-ws281x-go"
 )
 
-func main() {
-	err := rpio.Open()
-	defer rpio.Close()
-	if err != nil {
-		log.Fatal("GPIO Pins could not be initialised")
-	}
+const (
+	brightness = 90
+	ledCounts  = 64
+	sleepTime  = 50
+)
 
-	router := mux.NewRouter().StrictSlash(true)
-	router.HandleFunc("/", server.HomeLink)
-	router.HandleFunc("/colour/{colour}", server.GetColour).Methods("GET")
-	router.HandleFunc("/preset/{preset}", server.SetPreset).Methods("GET")
-	router.HandleFunc("/pin/{set}", server.SetPin).Methods("GET")
-	router.HandleFunc("/pcon", server.PowerOn).Methods("GET")
-	log.Fatal(http.ListenAndServe(":10000", router))
+type wsEngine interface {
+	Init() error
+	Render() error
+	Wait() error
+	Fini()
+	Leds(channel int) []uint32
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+type colorWipe struct {
+	ws wsEngine
+}
+
+func (cw *colorWipe) setup() error {
+	return cw.ws.Init()
+}
+
+func (cw *colorWipe) display(color uint32) error {
+	for i := 0; i < len(cw.ws.Leds(0)); i++ {
+		cw.ws.Leds(0)[i] = color
+		if err := cw.ws.Render(); err != nil {
+			return err
+		}
+		time.Sleep(sleepTime * time.Millisecond)
+	}
+	return nil
+}
+
+func main() {
+	opt := ws2811.DefaultOptions
+	opt.Channels[0].Brightness = brightness
+	opt.Channels[0].LedCount = ledCounts
+
+	dev, err := ws2811.MakeWS2811(&opt)
+	checkError(err)
+
+	cw := &colorWipe{
+		ws: dev,
+	}
+	checkError(cw.setup())
+	defer dev.Fini()
+
+	cw.display(uint32(0x0000ff))
+	cw.display(uint32(0x00ff00))
+	cw.display(uint32(0xff0000))
+	cw.display(uint32(0x000000))
+
 }
